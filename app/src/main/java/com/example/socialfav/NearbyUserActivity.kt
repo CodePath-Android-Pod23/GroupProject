e52e3597493654a335fd5c6eaaf6fdfe8696d3f0
Chenming Cao <chenmcao@seas.upenn.edu>
2022-04-22 14:41:20 -0600

    Update queries in NearbyUserActivity

--- app/src/main/java/com/example/socialfav/NearbyUserActivity.kt
+++ app/src/main/java/com/example/socialfav/NearbyUserActivity.kt
@@ -14,31 +14,36 @@ class NearbyUserActivity : AppCompatActivity() {
         setContentView(R.layout.activity_nearby_user)
     }
 
-    // 1. get the user's location and favorite genres
-
-    // 2. query to get users with the same location and at least one of the favorite genres
-
-
-    fun queryUsers(location: String, genres: List<String>) {
+    fun queryUsers() {
 //        adapter.clear()
+        // 1. get the user's location and favorite genres
+        val currentUser: ParseUser = ParseUser.getCurrentUser()
+        val location = currentUser.getString(User.KEY_LOCATION)
+        val genres = currentUser.getJSONArray(User.KEY_GENRES)
+        // 2. query to get users with the same location and at least one of the favorite genres
         // Specify which class to query
-        val query: ParseQuery<User> = ParseQuery.getQuery(User::class.java)
-        // Only return posts from current signed in user
-        query.whereEqualTo(User.KEY_LOCATION, location)
-        // return posts in descending order
-        query.addDescendingOrder("createdAt")
-
-        // only return the most recent 20 posts
-
-        query.findInBackground(object: FindCallback<User> {
-            override fun done(posts: MutableList<User>?, e: ParseException?) {
+        val queries = ArrayList<ParseQuery<User>>()
+        if (genres != null) {
+            for (i in 0 until genres.length()) {
+                val genre = genres.getJSONObject(i).toString()
+                val query: ParseQuery<User> = ParseQuery.getQuery(User::class.java)
+                // Return users with the same location as the current user
+                query.whereEqualTo(User.KEY_LOCATION, location)
+                query.whereEqualTo(User.KEY_GENRES, genre)
+                queries.add(query)
+            }
+        }
+        // Find users match at least one of the genres
+        val mainQuery: ParseQuery<User> = ParseQuery.or(queries)
+        mainQuery.findInBackground(object: FindCallback<User> {
+            override fun done(users: MutableList<User>?, e: ParseException?) {
                 if (e != null) {
                     // Something has went wrong
-                    Log.e(MainActivity.TAG, "Error fetching posts")
+                    Log.e(MainActivity.TAG, "Error fetching users")
                 } else {
-                    if (posts != null) {
-                        for (post in posts) {
-//                            Log.i(MainActivity.TAG, "Post: " + post.getDescription() + " , username: " + post.getUser()?.username)
+                    if (users != null) {
+                        for (user in users) {
+                            Log.i(MainActivity.TAG, "User: " + user.getFullName() + " , Location: " + user.getLocation())
                         }
 //                        allPosts.addAll(posts)
 //                        adapter.notifyDataSetChanged()
