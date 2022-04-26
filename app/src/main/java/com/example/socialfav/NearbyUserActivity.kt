2eb353cc1ba52ff41b95d1d5b6b79e6b6dc2802f
Chenming Cao <chenmcao@seas.upenn.edu>
2022-04-25 23:07:40 -0600

    Update NearbyUser query, exclude current user from result

--- app/src/main/java/com/example/socialfav/NearbyUserActivity.kt
+++ app/src/main/java/com/example/socialfav/NearbyUserActivity.kt
@@ -17,30 +17,31 @@ class NearbyUserActivity : AppCompatActivity() {
 
     private fun queryUsers() {
 //        adapter.clear()
-        // 1. get the user's location and favorite genres
+        // 1. get the current user's location and favorite genres
         val currentUser: ParseUser = ParseUser.getCurrentUser()
         val location = currentUser.getString("Location")
         val genres = currentUser.getJSONArray("Genres")
+        val id = currentUser.objectId
         Log.i(TAG, "Current user location is " + location)
         Log.i(TAG, "genres size is " + genres?.length())
         // 2. query to get users with the same location and at least one of the favorite genres
-        // Specify which class to query
         val queries = ArrayList<ParseQuery<ParseUser>>()
         if (genres != null) {
             for (i in 0 until genres.length()) {
                 val genre = genres.getString(i)
-//                Log.i(TAG, "Genre is " + genre)
                 val query: ParseQuery<ParseUser> = ParseUser.getQuery()
-                // Return users with the same location as the current user
+                // Match location
                 query.whereEqualTo("Location", location)
+                // Match Genre
                 query.whereEqualTo("Genres", genre)
+                // Exclude current user
+                query.whereNotEqualTo("objectId", id)
                 queries.add(query)
             }
         }
-        // Find users match at least one of the genres
+        // Find users match at least one of the genres use compound queries
         val mainQuery: ParseQuery<ParseUser> = ParseQuery.or(queries)
         mainQuery.findInBackground(object: FindCallback<ParseUser> {
-//            query.findInBackground(object: FindCallback<ParseUser> {
             override fun done(users: MutableList<ParseUser>?, e: ParseException?) {
                 if (e != null) {
                     // Something has went wrong
