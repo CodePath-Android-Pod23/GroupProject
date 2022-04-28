039dbdec0ec55742c633b9a6f8b6e3616e32ccf3
Pijamo <ngulliejake@gmail.com>
2022-04-29 01:07:43 +0530

    Created Settings Modal Fragment Completed Profile Skeleton

---
+++ app/src/main/java/com/example/socialfav/fragments/ModalBottomSheet.kt
@@ -0,0 +1,28 @@
+package com.example.socialfav.fragments
+
+import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.socialfav.R
+
+
+
+class ModalBottomSheet : BottomSheetDialogFragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_modal_bottom_sheet_list_dialog, container, false)
+    }
+
+    companion object {
+        const val TAG = "ModalBottomSheet"
+    }
+}
